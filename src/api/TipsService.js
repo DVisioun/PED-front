ab67a992d3a8ebfbe4d6b528ab11807049575d54
Luana <santos.luanabeatriz1995@gmail.com>
2024-04-08 09:42:55 +0100

    Update TipsService.js

--- src/api/TipsService.js
+++ src/api/TipsService.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-const BASE_URL = "http://localhost:3001/tips";
+const BASE_URL = 'http://localhost:3001/tips';
 
 export class TipsService {
     static get() {
